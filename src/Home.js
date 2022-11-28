6828cafef8eb78935fa1252e24fd057b8b112aa9
Eliyas Kassaye <eliyasson11@gmail.com>
2022-11-28 12:12:48 +0200

    reusing components

--- src/Home.js
+++ src/Home.js
@@ -13,12 +13,13 @@ const Home = () => {
     const [blogs, setBlogs] =useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Eliyas', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Tara', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Lucas', id: 3 }
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Eliyas', id: 3 }
     ])
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Eliyas')} title="Eliyas's blogs"/>
         </div>
 
      );
