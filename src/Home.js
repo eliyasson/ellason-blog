1308e52277b70767469740b36af44c8476431964
Eliyas Kassaye <eliyasson11@gmail.com>
2022-11-28 10:21:08 +0200

    props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     //let name = 'eliyas';
@@ -10,20 +11,14 @@ const Home = () => {
     //     setAge(30);
     // }
     const [blogs, setBlogs] =useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        { title: 'My new website', body: 'lorem ipsum...', author: 'Eliyas', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Tara', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Lucas', id: 3 }
     ])
 
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
-
+            <BlogList blogs={blogs} title="All Blogs!"/>
         </div>
 
      );
