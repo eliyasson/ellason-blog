2f4b1d4e4dd19097c6e983aac4a51037a708dce3
Eliyas Kassaye <eliyasson11@gmail.com>
2022-11-30 21:43:27 +0200

    commit

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    //let name = 'eliyas';
-    // const [name, setName] = useState('Lucy');
-    // const [age, setAge] = useState(20);
-    
-    // const handleClick = () => {
-    //     setName('Eliyas');
-    //     setAge(30);
-    // }
+  
     const [blogs, setBlogs] =useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Eliyas', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Tara', id: 2 },
