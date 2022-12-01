b522cfd2cb673960108cf40210f0010db34b431c
Eliyas Kassaye <eliyasson11@gmail.com>
2022-12-01 23:07:41 +0200

    fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,6 @@ import BlogList from "./BlogList";
 const Home = () => {
    const [blogs, setBlogs] =useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    };
-
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
@@ -16,13 +11,14 @@ const Home = () => {
         })
         .then(data => {
             console.log(data);
+            setBlogs(data);
         })
         
     }, []);
 
     return ( 
         <div className="home">
-            
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
         </div>
 
      );
